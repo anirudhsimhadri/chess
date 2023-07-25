eea5c480c964e3f0320e797e50f95ddbb3199f05
Sam Aven <sam.aven@priglobal.com>
2023-07-25 09:30:47 -0500

    added matrix above gameboard

--- gameBoard/gameboard.py
+++ gameBoard/gameboard.py
@@ -27,12 +27,36 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
-#create screen
-def draw_chessboard(screen):
-    for row in range(8):
-        for col in range(8):
-            color = WHITE if (row + col) % 2 == 0 else BLACK
-            pygame.draw.rect(screen, color, (col * SQUARE_SIZE + OFFSET_X, row * SQUARE_SIZE + OFFSET_Y, SQUARE_SIZE, SQUARE_SIZE))
+class ChessboardMatrix:
+    def __init__(self):
+        #define the rows and columns of the matrix
+        self.rows = 8
+        self.cols = 8
+
+        self.chessboard = [['-'for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def place_piece(self, row, col, piece):
+        self.chessboard[row][col] = piece
+
+    def draw_piece(self, screen):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                piece = self.chessboard[row][col]
+                if piece != '-':
+                    x = col * SQUARE_SIZE + OFFSET_X + SQUARE_SIZE // 2
+                    y = row * SQUARE_SIZE + OFFSET_Y + SQUARE_SIZE // 2
+                    font = pygame.font.SysFont('Arial', 30)
+                    text_surface = font.render(piece, True, RED)
+                    text_rect = text_surface.get_rect(center=(x, y))
+                    screen.blit(text_surface, text_rect)
+    
+    #create screen
+    def draw_chessboard(self, screen):
+        for row in range(8):
+            for col in range(8):
+                color = WHITE if (row + col) % 2 == 0 else BLACK
+                pygame.draw.rect(screen, color, (col * SQUARE_SIZE + OFFSET_X, row * SQUARE_SIZE + OFFSET_Y, SQUARE_SIZE, SQUARE_SIZE))
+
 
 def draw_tracking_boxes(screen):
     """Draws the material tracking boxes on the screen"""
@@ -53,6 +77,8 @@ def main():
     screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
     pygame.display.set_caption("Chess Board")
 
+    chessboard_matrix = ChessboardMatrix()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -63,7 +89,10 @@ def main():
         screen.fill(RED)
 
         #Draw the chessboard
-        draw_chessboard(screen)
+        chessboard_matrix.draw_chessboard(screen)
+
+        #Draw the pieces
+        chessboard_matrix.draw_piece(screen)
 
         #Draw the material tracking boxes
         draw_tracking_boxes(screen)
