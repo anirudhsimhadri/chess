99d23094005d440cb9e447cc3bbc98a3c67b6a9b
Sam Aven <sam.aven@priglobal.com>
2023-08-01 10:30:18 -0500

    cleaned up knights code

--- gameBoard/knight.py
+++ gameBoard/knight.py
@@ -18,21 +18,27 @@ class Knight():
         self.image = pygame.transform.scale(self.image, (self.cs.SQUARE_SIZE, self.cs.SQUARE_SIZE))
 
     def is_valid_move(self, row, col, target_row, target_col, matrix: cbm):
-        if target_col == col - 1 and target_row == row + 2 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color != matrix.chessboard[row][col].color):
+        """checks if the desired move is valid"""
+
+        #checks if the target location is valid
+        location = (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color != matrix.chessboard[row][col].color)
+
+        #checking if the designated location is valid according to knights movement rules
+        if target_col == col - 1 and target_row == row + 2 and (location):
             return True
-        elif target_col == col - 1 and target_row == row - 2 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color != matrix.chessboard[row][col].color):
+        elif target_col == col - 1 and target_row == row - 2 and (location):
             return True
-        elif target_col == col - 2 and target_row == row + 1 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color != matrix.chessboard[row][col].color):
+        elif target_col == col - 2 and target_row == row + 1 and (location):
             return True
-        elif target_col == col - 2 and target_row == row - 1 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color != matrix.chessboard[row][col].color):
+        elif target_col == col - 2 and target_row == row - 1 and (location):
             return True
-        elif target_col == col + 1 and target_row == row + 2 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color!= matrix.chessboard[row][col].color):
+        elif target_col == col + 1 and target_row == row + 2 and (location):
             return True
-        elif target_col == col + 1 and target_row == row - 2 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color!= matrix.chessboard[row][col].color):
+        elif target_col == col + 1 and target_row == row - 2 and (location):
             return True
-        elif target_col == col + 2 and target_row == row + 1 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color!= matrix.chessboard[row][col].color):
+        elif target_col == col + 2 and target_row == row + 1 and (location):
             return True
-        elif target_col == col + 2 and target_row == row - 1 and (matrix.chessboard[target_row][target_col] == None or matrix.chessboard[target_row][target_col].color!= matrix.chessboard[row][col].color):
+        elif target_col == col + 2 and target_row == row - 1 and (location):
             return True
         else:
             return False
