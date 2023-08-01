7b3c19cd68ebaff20e7a5245f0849e4df4a49ce7
Ani Simhadri <anirudhsimhadri@gmail.com>
2023-08-01 15:31:30 -0500

    Up Down Left Right movements from rooks

--- gameBoard/queen.py
+++ gameBoard/queen.py
@@ -20,22 +20,22 @@ class Queen():
     def move(self, x, y):
         """Queen move function"""
 
-    def is_valid_move(self, start_row, start_col, end_row, end_col, matrix: cbm) -> bool:
+    def is_valid_move_diagonally(self, start_row, start_col, end_row, end_col, matrix: cbm) -> bool:
         row_diff = end_row - start_row
         col_diff = end_col - start_col
-        # # Check if the queen can move to the target location
-        # delta_row = target_row - row
-        # delta_col = target_col - col
+        # Check if the queen can move to the target location
+        delta_row = target_row - row
+        delta_col = target_col - col
 
-        # # Queen moves along a rank, file, or diagonal
-        # if abs(delta_row) == abs(delta_col) or row == target_row or col == target_col:
-        #     # Check if the path is clear
-        #     path_rows = range(row + delta_row//abs(delta_row), target_row, delta_row//abs(delta_row)) if delta_row != 0 else [row]*abs(delta_col)
-        #     path_cols = range(col + delta_col//abs(delta_col), target_col, delta_col//abs(delta_col)) if delta_col != 0 else [col]*abs(delta_row)
-        #     for r, c in zip(path_rows, path_cols):
-        #         if matrix.chessboard[r][c]:
-        #             return False
-        #     return True
+        # Queen moves along a rank, file, or diagonal
+        if abs(delta_row) == abs(delta_col) or row == target_row or col == target_col:
+            # Check if the path is clear
+            path_rows = range(row + delta_row//abs(delta_row), target_row, delta_row//abs(delta_row)) if delta_row != 0 else [row]*abs(delta_col)
+            path_cols = range(col + delta_col//abs(delta_col), target_col, delta_col//abs(delta_col)) if delta_col != 0 else [col]*abs(delta_row)
+            for r, c in zip(path_rows, path_cols):
+                if matrix.chessboard[r][c]:
+                    return False
+            return True
         if abs(row_diff) != abs(col_diff): return False
         if end_row >= 8 or end_col >= 8: return False
 
@@ -59,3 +59,15 @@ class Queen():
 
         return True
         return False
+
+    def is_valid_move_up_down_left_right(self, row, col, target_row, target_col, board):
+        """
+        Checks if the move is valid (it's a straight line and no piece is blocking the path).
+        This doesn't check if the king would be in check after this move - that logic belongs elsewhere.
+        """
+        if target_row == row and (target_col in range(8)) and (board.chessboard[target_row][target_col] == None or board.chessboard[target_row][target_col].color != board.chessboard[row][col].color):
+            return True
+        elif target_col == col and (target_row in range(8)) and (board.chessboard[target_row][target_col] == None or board.chessboard[target_row][target_col].color != board.chessboard[row][col].color):
+            return True
+        else:
+            return False
