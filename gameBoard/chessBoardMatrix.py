880899d2a5eb95cb242573931f4dc75fc9f751b6
Andrew Voss <andrew.voss@priglobal.com>
2023-08-03 12:31:11 -0500

    Echo moves over network (BUGGED)

--- gameBoard/chessBoardMatrix.py
+++ gameBoard/chessBoardMatrix.py
@@ -8,38 +8,39 @@ Surface = pygame.Surface
 class ChessBoardMatrix:
     chessboard: list[list[ChessPiece | None]]
 
-    def __init__(self):
+    def __init__(self, is_client: bool):
         #define the rows and columns of the matrix
         self.rows = 8
         self.cols = 8
 
         self.chessboard = [[None for _ in range(self.cols)] for _ in range(self.rows)]
         
-        #load images
-        self.pawn_images ={
-            'white': pygame.image.load('white_pawn.png').convert_alpha(),
-            'black': pygame.image.load('black_pawn.png').convert_alpha()
-        }
-        self.rook_images ={
-            'white': pygame.image.load('white_rook.png').convert_alpha(),
-            'black': pygame.image.load('black_rook.png').convert_alpha()
-        }
-        self.bishop_images ={
-            'white': pygame.image.load('white_bishop.png').convert_alpha(),
-            'black': pygame.image.load('black_bishop.png').convert_alpha()
-        }
-        self.queen_images ={
-            'white': pygame.image.load('white_queen.png').convert_alpha(),
-            'black': pygame.image.load('black_queen.png').convert_alpha()
-        }
-        self.king_images ={
-            'white': pygame.image.load('white_king.png').convert_alpha(),
-            'black': pygame.image.load('black_king.png').convert_alpha()
-        }
-        self.knight_images ={
-            'white': pygame.image.load('white_knight.png').convert_alpha(),
-            'black': pygame.image.load('black_knight.png').convert_alpha()
-        }
+        if is_client:
+            #load images
+            self.pawn_images ={
+                'white': pygame.image.load('white_pawn.png').convert_alpha(),
+                'black': pygame.image.load('black_pawn.png').convert_alpha()
+            }
+            self.rook_images ={
+                'white': pygame.image.load('white_rook.png').convert_alpha(),
+                'black': pygame.image.load('black_rook.png').convert_alpha()
+            }
+            self.bishop_images ={
+                'white': pygame.image.load('white_bishop.png').convert_alpha(),
+                'black': pygame.image.load('black_bishop.png').convert_alpha()
+            }
+            self.queen_images ={
+                'white': pygame.image.load('white_queen.png').convert_alpha(),
+                'black': pygame.image.load('black_queen.png').convert_alpha()
+            }
+            self.king_images ={
+                'white': pygame.image.load('white_king.png').convert_alpha(),
+                'black': pygame.image.load('black_king.png').convert_alpha()
+            }
+            self.knight_images ={
+                'white': pygame.image.load('white_knight.png').convert_alpha(),
+                'black': pygame.image.load('black_knight.png').convert_alpha()
+            }
 
         self.cs = BoardConstants()
 
--- gameBoard/server.py
+++ gameBoard/server.py
@@ -1,11 +1,89 @@
 from typing import Tuple
 import socket
 import random
+import net
+from chessBoardMatrix import ChessBoardMatrix
+from typedef import ChessBoardMatrix as Cbm
+from typing import cast
+from pawn import Pawn
+from rook import Rook
+from bishop import Bishop
+from knight import Knight
+from queen import Queen
+from king import King
 
 Socket = socket.socket
 
 class BadConnection(BaseException): pass
 class PasswordMismatch(BaseException): pass
+
+def runGame(p1: Socket, p2: Socket, p1IsWhite: bool):
+    p1Turn = p1IsWhite
+
+    matrix = ChessBoardMatrix(False)
+
+    for col in range(matrix.cols):
+        matrix.place_piece(6, col, Pawn("white"))
+
+    for col in range(matrix.cols):
+        matrix.place_piece(1, col, Pawn("black"))
+    
+    matrix.place_piece(0, 0, Rook("black"))
+    matrix.place_piece(7, 7, Rook("white"))
+    matrix.place_piece(0, 7, Rook("black"))
+    matrix.place_piece(7, 0, Rook("white"))
+
+    matrix.place_piece(0, 1, Bishop("black"))
+    matrix.place_piece(7, 6, Bishop("white"))
+    matrix.place_piece(0, 6, Bishop("black"))
+    matrix.place_piece(7, 1, Bishop("white"))
+    
+    matrix.place_piece(0, 2, Knight('black'))
+    matrix.place_piece(7, 5, Knight('white'))
+    matrix.place_piece(0, 5, Knight('black'))
+    matrix.place_piece(7, 2, Knight('white'))
+    
+    matrix.place_piece(0, 3, Queen('black'))
+    matrix.place_piece(7, 3, Queen('white'))
+
+    matrix.place_piece(0, 4, King('black'))
+    matrix.place_piece(7, 4, King('white'))
+
+    game_ended = False
+
+    while not game_ended:
+        conn = p1 if p1Turn else p2
+        move_start, move_end = net.recvMove(conn)
+        print("P1" if p1Turn else "P2", f"moved from {move_start} to {move_end}")
+        piece = matrix.chessboard[move_start[0]][move_start[0]]
+        target = matrix.chessboard[move_end[0]][move_end[1]]
+
+        if piece is None:
+            conn.send(b"invalid!")
+            continue
+
+        if not piece.is_valid_move(*move_start, *move_end, cast(Cbm, matrix)):
+            conn.send(b"invalid!")
+            continue
+
+        matrix.move(*move_start, *move_end)
+
+        if matrix.is_king_in_check(piece.color):
+            conn.send(b"invalid!")
+            matrix.undo_move(*move_start, *move_end, target)
+            continue
+
+        opp_color = "white" if piece.color == "black" else "black"
+
+        if matrix.is_checkmate(opp_color):
+            game_ended = True
+
+        oppConn = p2 if p1Turn else p1
+        conn.send(b"valid!!!")
+        net.sendMove(oppConn, move_start, move_end)
+        oppConn.send(b"yourmove" if not game_ended else b"gameover")
+        
+        p1Turn = not p1Turn
 
 def acceptPlayer(sock: Socket, password: str) -> Tuple[Socket, str]:
     conn, addr = sock.accept()
@@ -72,9 +150,13 @@ def serve(port: int, password: str):
     
     print("P2 connected with name {}".format(p2Name))
 
-    p1isWhite = bool(random.getrandbits(1))
+    p1IsWhite = bool(random.getrandbits(1))
 
-    p1Conn.send(b"white!!!" if p1isWhite else b"black!!!")
-    p2Conn.send(b"white!!!" if not p1isWhite else b"black!!!")
+    p1Conn.send(b"white!!!" if p1IsWhite else b"black!!!")
+    p2Conn.send(b"white!!!" if not p1IsWhite else b"black!!!")
+
+    print("P1 is white" if p1IsWhite else "P2 is white")
+
+    runGame(p1Conn, p2Conn, p1IsWhite)
 
 if __name__ == "__main__": serve(4567, "password")
